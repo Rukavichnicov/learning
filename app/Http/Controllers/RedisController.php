2fd8aa6eedfeaba7cc89c6b2f666b18db44e2d99
Sergey Rukavichnikov <serega1998love@yandex.ru>
2023-01-16 21:57:08 +0500

    Learning strings in redis, setting, receiving, setting the lifetime

--- app/Http/Controllers/RedisController.php
+++ app/Http/Controllers/RedisController.php
@@ -2,25 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\View\View;
+use Redis;
+use RedisException;
+
 class RedisController extends Controller
 {
     /**
-     * @throws \RedisException
+     * @throws RedisException
      */
-    public function index()
+    public function index(): View
     {
-        $redis = new \Redis();
+        $redis = new Redis();
+        $array = [];
         $redis->pconnect(
             env('REDIS_HOST', '127.0.0.1'),
             env('REDIS_PORT', '6379'),
         );
-        $redis->set('name:1', 'Misha');
-        $redis->set('name:2', 'Sergey');
-        $redis->set('name:3', 'Dasha');
-        $array = [];
-        $array[] = $redis->get('name:1');
-        $array[] = $redis->get('name:2');
-        $array[] = $redis->get('name:3');
+        $redis->mSet(
+            [
+                'age:1' => 24,
+                'age:2' => 50,
+                'age:3' => 80,
+            ]
+        );
+        for ($i = 1; $i <= 3; ++$i) {
+            $redis->set("name:$i", "Misha$i", 10);
+            $redis->setnx('name:1', 'Mom');
+            $redis->persist('name:2');
+            $redis->expire('name:3', 3600);
+        }
+        $redis->incr('age:1');
+        $redis->decr('age:2');
+        for ($i = 1; $i <= 3; ++$i) {
+            $array["getName$i"] = $redis->get("name:$i");
+            $array["ttlName$i"] = $redis->ttl("name:$i");
+        }
+        $array = array_merge($array, $redis->mGet(["age:1", "age:2", "age:3",]));
+
+
+        $redis->close();
         return view('redis', compact('array'));
     }
 }
--- resources/views/redis.blade.php
+++ resources/views/redis.blade.php
@@ -4,23 +4,26 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Redis</title>
-
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </head>
     <body>
-        <h1>Learning redis!</h1>
-        <table>
-            <thead>
+        <div class="content m-5">
+            <h1>Learning redis!</h1>
+            <table class="table">
+                <thead>
                 <th>Key</th>
                 <th>Value</th>
-            </thead>
-            <tbody>
+                </thead>
+                <tbody>
                 @foreach($array as $key => $value)
                     <tr>
                         <td>{{ $key }}</td>
                         <td>{{ $value }}</td>
                     </tr>
                 @endforeach
-            </tbody>
-        </table>
+                </tbody>
+            </table>
+        </div>
     </body>
 </html>
