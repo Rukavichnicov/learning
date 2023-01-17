1d2787a7b48b9df7879b1715be7add14aec35522
Sergey Rukavichnikov <serega1998love@yandex.ru>
2023-01-17 22:01:08 +0500

    Learning sets, sorted sets and hash in redis, setting, receiving

--- app/Http/Controllers/RedisController.php
+++ app/Http/Controllers/RedisController.php
@@ -39,18 +39,49 @@ class RedisController extends Controller
 //            $array["ttlName$i"] = $redis->ttl("name:$i");
 //        }
 //        $array = array_merge($array, $redis->mGet(["age:1", "age:2", "age:3",]));
-        $redis->lPush('list:1', 4);
-        $redis->lPush('list:1', 3);
-        $redis->lPush('list:1', 2);
-        $redis->lPush('list:1', 1);
-        $redis->lPush('list:2', 4, 2, 3);
-        $redis->lTrim('list:1', 0, 2);
-        $array[] = $redis->rPop('list:1');
-        $array[] = $redis->rPop('list:1');
-        $array[] = $redis->rPop('list:1');
-        $array['list1'] = $redis->lLen('list:1');
-        $array['list2'] = $redis->lLen('list:2');
-        $redis->del('list:2');
+//        $redis->lPush('list:1', 4);
+//        $redis->lPush('list:1', 3);
+//        $redis->lPush('list:1', 2);
+//        $redis->lPush('list:1', 1);
+//        $redis->lPush('list:2', 4, 2, 3);
+//        $redis->lTrim('list:1', 0, 2);
+//        $array[] = $redis->rPop('list:1');
+//        $array[] = $redis->rPop('list:1');
+//        $array[] = $redis->rPop('list:1');
+//        $array['list1'] = $redis->lLen('list:1');
+//        $array['list2'] = $redis->lLen('list:2');
+//        $redis->del('list:2');
+//
+//        $redis->set('foo', 'bar');
+//
+//        // Получить значение по ключу, и перезаписать его новым значением
+//        $array['foo'] = $redis->getSet('foo', 'new');
+//        $array['foo_new'] = $redis->get('foo');
+
+//        $redis->sAdd('set:1', 1);
+//        $redis->sAdd('set:1', 2);
+//        $redis->sAdd('set:1', 3);
+//        $redis->sAdd('set:1', 4);
+//
+//        $redis->sAdd('set:2', 10);
+//        $redis->sAdd('set:2', 11);
+//        $redis->sAdd('set:2', 12);
+//        $redis->sAdd('set:2', 15);
+//
+
+//        $redis->hSet('hash:1', 'name', 'Misha');
+//        $redis->hSet('hash:1', 'age', '24');
+//        $redis->hSet('hash:1', 'email', 'local@local.com');
+//        $redis->hMSet('hash:1', ['age' => '24', 'email' => 'local@local.com']);
+//        $redis->hIncrBy('hash:1', 'age', 10);
+//        dd($redis->hGetAll('hash:1'));
+
+//        $redis->zAdd('sorted:set:1', 100, 'Vasya');
+//        $redis->zAdd('sorted:set:1', 500, 'Petya');
+//        $redis->zAdd('sorted:set:1', 50, 'Misha');
+
+//        dd($redis->zRange('sorted:set:1', 0, -1, true));
+//        dd($redis->zRevRank('sorted:set:1', 'Petya'));
 
         $redis->close();
         return view('redis', compact('array'));
